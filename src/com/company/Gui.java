8d4632ce37b01f3bcec0e2f7c2bb1051532f37df
Chateux1 <m.satas@gmail.com>
2018-10-12 19:07:31 +0300

    Refactoring button creation and placement

--- src/com/company/Gui.java
+++ src/com/company/Gui.java
@@ -14,12 +14,18 @@ public class Gui extends JFrame implements ComponentListener, ItemListener {
     private JPanel gamePanel;
     public ArrayList<ImageIcon> listIcons;
     public HashMap<Integer, Button> table1;
-    public ArrayList<ArrayList<String>> listAllOut;
-    public ArrayList<String> listAllIn;
+    public ArrayList<ArrayList<String>> listButtonOuter;
+    public ArrayList<String> listButtonInner;
     public ArrayList<ArrayList<Button>> table;
     public ArrayList<Button> listCol, listAll;
     private ImageIcon explodeIcon, bombIcon, buttonIcon;
     private int buttonSize = 40;
+    private String[] IconPaths = {"images/button.png",
+            "images/bomb.png",
+            "images/explode.png"};
+    private String[] IconNames = { "button",
+            "bomb",
+            "explode"};
 
     public Gui(int row, int col, int mineCount) {
         super("Minesweeper");
@@ -27,6 +33,54 @@ public class Gui extends JFrame implements ComponentListener, ItemListener {
         this.col = col;
         this.mineCount = mineCount;
     }
+
+    public void setImages(int x, int y) {
+
+        this.listIcons = new ArrayList<>();
+        int scalingMethod = Image.SCALE_DEFAULT;
+
+        for (int i = 0; i < this.IconPaths.length; i++) {
+            ImageIcon tempIcon = new ImageIcon(IconPaths[i]);
+            Image tempImg = tempIcon.getImage();
+            Image tempResized = tempImg.getScaledInstance(x, y, scalingMethod);
+            this.listIcons.add(i, new ImageIcon(tempResized));
+        }
+    }
+
+    public void setButtonList() {
+
+        this.listButtonOuter = new ArrayList<>();
+        int id = 0;
+        for ( int i = 0; i < getRow(); i++ ) {
+
+            for ( int j = 0; j < getCol(); j++ ) {
+
+                this.table1.put(i, new Button(i, j, id));
+                id += 1;
+
+                this.listButtonInner = new ArrayList<>();
+                this.listButtonInner.add(Integer.toString(this.table1.get(id).getId()));
+                this.listButtonInner.add(Double.toString(this.table1.get(id).getMineExpectancy()));
+
+                //this.listButtonInner.add(Boolean.toString(table1.get(id).getMine()));
+            }
+            this.listButtonOuter.add(this.listButtonInner);
+        }
+    }
+
+    public void sortList() {
+        Collections.sort(this.listButtonOuter, new CompareMineExpectancies());
+    }
+
+    public void setMines() {
+        this.sortList();
+        for (int i = 0; i < this.getMineCount(); i++) {
+            int id = Integer.parseInt(listButtonOuter.get(i).get(1));
+            table1.get(id).setMine();
+        }
+
+    }
+
 
     public int getRow() {
         return this.row;
@@ -46,14 +100,7 @@ public class Gui extends JFrame implements ComponentListener, ItemListener {
         this.setResizable(false);
     }
 
-    public void setMines() {
-        this.sortList();
-        for (int i = 0; i < this.getMineCount(); i++) {
-            int id = Integer.parseInt(listAllOut.get(i).get(1));
-            table1.get(id).setMine();
-        }
 
-    }
 
     public void printHash() {
         System.out.println();
@@ -63,47 +110,13 @@ public class Gui extends JFrame implements ComponentListener, ItemListener {
             System.out.println(")");
         }
     }
-    public void sortList() {
-        Collections.sort(this.listAllOut, new CompareMineExpectancies());
-    }
+
 
     public void setLayout() {
         this.setLayout(new GridBagLayout());
     }
 
-    public void setImages(int x, int y) {
 
-        this.listIcons = new ArrayList<>();
-        int scalingMethod = Image.SCALE_DEFAULT;
-
-        String[] paths = {"images/button.png",
-                "images/bomb.png",
-                "images/explode.png"};
-        String[] names = { "button",
-                        "bomb",
-                        "explode"};
-
-        for (int i = 0; i < paths.length; i++) {
-            ImageIcon tempIcon = new ImageIcon(paths[i]);
-            Image tempImg = tempIcon.getImage();
-            Image tempResized = tempImg.getScaledInstance(x, y, scalingMethod);
-            this.listIcons.add(i, new ImageIcon(tempResized));
-        }
-
-
-        ImageIcon bombIcon = new ImageIcon("images/bomb.png");
-        Image bombImage = bombIcon.getImage();
-        Image bombResizedImg = bombImage.getScaledInstance(x, y, Image.SCALE_DEFAULT);
-        this.bombIcon = new ImageIcon(bombResizedImg);
-        ImageIcon explodeIcon = new ImageIcon("images/explode.png");
-        Image explodeImage = explodeIcon.getImage();
-        Image explodeResizedImg = explodeImage.getScaledInstance(x, y, Image.SCALE_DEFAULT);
-        this.explodeIcon = new ImageIcon(explodeResizedImg);
-        ImageIcon buttonIcon = new ImageIcon("images/button.png");
-        Image buttonImage = buttonIcon.getImage();
-        Image buttonResizedImg = buttonImage.getScaledInstance(x, y, Image.SCALE_DEFAULT);
-        this.buttonIcon = new ImageIcon(buttonResizedImg);
-    }
 
     public void setMainPanel() {
 
@@ -113,8 +126,14 @@ public class Gui extends JFrame implements ComponentListener, ItemListener {
         this.table = new ArrayList<>();
         this.listAll = new ArrayList<>();
         this.table1 = new HashMap<>();
-        this.listAllOut = new ArrayList<>();
+        this.listButtonOuter = new ArrayList<>();
 
+        for (int i = 0; i < this.getRow(); i++) {
+            for (int j = 0; j < this.getCol(); j++) {
+
+
+            }
+        }
 
         for (int row = 0; row < this.getRow(); row++) {
 
@@ -122,23 +141,25 @@ public class Gui extends JFrame implements ComponentListener, ItemListener {
 
             for (int col = 0; col < this.getCol(); col++) {
 
-                this.listAllIn = new ArrayList<>();
+                this.listButtonInner = new ArrayList<>();
 
                 Button btn = new Button(row, col, counter);
                 btn.setBounds(row * btn.getButtonSize(),
                         col * btn.getButtonSize(),
                         btn.getButtonSize(),btn.getButtonSize());
-
-                btn.setIcon(this.buttonIcon);
-                btn.setSelectedIcon(this.explodeIcon);
-                this.listAllIn.add(Double.toString(btn.getMineExpectancy()));
-                this.listAllIn.add(Integer.toString(btn.getId()));
-                this.listAllIn.add(Boolean.toString(btn.getMine()));
+                this.setImages(40,40);
+                btn.setIcon(this.listIcons.get(0));
+                btn.setSelectedIcon(this.listIcons.get(2));
+                //btn.setIcon(this.buttonIcon);
+                //btn.setSelectedIcon(this.explodeIcon);
+                this.listButtonInner.add(Double.toString(btn.getMineExpectancy()));
+                this.listButtonInner.add(Integer.toString(btn.getId()));
+                this.listButtonInner.add(Boolean.toString(btn.getMine()));
                 this.listCol.add(btn);
                 this.table1.put(counter, btn);
                 counter +=1;
                 gamePanel.add(this.listCol.get(col));
-                this.listAllOut.add(this.listAllIn);
+                this.listButtonOuter.add(this.listButtonInner);
             }
 
             this.table.add(this.listCol);
@@ -149,11 +170,11 @@ public class Gui extends JFrame implements ComponentListener, ItemListener {
 
     public void printList() {
         System.out.println();
-        for (int i = 0; i < this.listAllOut.size(); i++) {
+        for (int i = 0; i < this.listButtonOuter.size(); i++) {
             System.out.println();
-            System.out.print("(" + this.listAllOut.get(i).get(0));
-            System.out.print(", " + this.listAllOut.get(i).get(1));
-            System.out.print(", " + this.listAllOut.get(i).get(2) + ")");
+            System.out.print("(" + this.listButtonOuter.get(i).get(0));
+            System.out.print(", " + this.listButtonOuter.get(i).get(1));
+            System.out.print(", " + this.listButtonOuter.get(i).get(2) + ")");
         }
     }
 
@@ -175,7 +196,7 @@ public class Gui extends JFrame implements ComponentListener, ItemListener {
 
     public void showGui() {
         this.setLayout();
-        this.setImages(40,40);
+        //this.setImages(40,40);
         this.setMainPanel();
         this.add(gamePanel);
         this.setSettings();
